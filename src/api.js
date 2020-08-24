48c26efcdc8c441c4ce6f780fb37f784f04fae2f
Ian Bicking <ian@ianbicking.org>
2020-08-23 23:20:35 -0500

    Try to build a packaged version for the browser

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import { convertEntities, splitPhraseLines, compile } from "./compiler.js";
+import { PhraseSet } from "./findMatch.js";
+import * as textMatching from "./textMatching.js";
+
+export default {
+  convertEntities,
+  splitPhraseLines,
+  compile,
+  PhraseSet,
+  textMatching,
+};
